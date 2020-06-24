84751584ce90826d5d8a479fbad9cf3f69f7dea7
SunWuFannn <54068109+SunWuFannn@users.noreply.github.com>
2020-06-25 02:33:54 +0800

    Update Category.java

--- src/main/java/com/swf/petshop/domain/Category.java
+++ src/main/java/com/swf/petshop/domain/Category.java
@@ -6,6 +6,8 @@ import java.io.Serializable;
  * 分类实体类
  */
 public class Category implements Serializable {
+    
+    private static final long serialVersionUID = 1L;
 
     private int cid;//分类id
     private String cname;//分类名称
