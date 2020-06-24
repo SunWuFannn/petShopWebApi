79a8d2c04bead1a8e76dbb2d7ce682ed0ea44bf2
SunWuFannn <54068109+SunWuFannn@users.noreply.github.com>
2020-06-25 02:35:37 +0800

    Update ResultInfo.java

--- src/main/java/com/swf/petshop/domain/ResultInfo.java
+++ src/main/java/com/swf/petshop/domain/ResultInfo.java
@@ -7,6 +7,9 @@ import java.util.Objects;
  * 用于封装后端返回前端数据对象
  */
 public class ResultInfo implements Serializable {
+    
+    private static final long serialVersionUID = 1L;
+    
     private boolean flag;//后端返回结果正常为true，发生异常返回false
     private Object data;//后端返回结果数据对象
     private String errorMsg;//发生异常的错误消息
